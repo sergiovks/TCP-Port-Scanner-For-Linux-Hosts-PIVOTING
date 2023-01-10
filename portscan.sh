f7b84d5e771563fca412ad4f61febe3b47141d55
SezioS <118485549+sergiovks@users.noreply.github.com>
2023-01-10 20:04:39 +0100

    Update portscan.sh

--- portscan.sh
+++ portscan.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function ctrl_c(){
-        echo -e "\n\n[!] Saliendo...\n"
+        echo -e "\n\n[!] Leaving...\n"
         tput cnorm; exit 1
 }
 
@@ -14,7 +14,7 @@ tput civis
 # Change the IP address for the IP that you want to scan to pivot
 
 for i in $(seq 1 65535); do
-        timeout 1 bash -c "echo '' > /dev/tcp/10.10.0.128/$port" 2>/dev/null && echo "[+] El puerto $port esta abierto" &
+        timeout 1 bash -c "echo '' > /dev/tcp/10.10.0.128/$port" 2>/dev/null && echo "[+] The port $port is open" &
 done; wait
 
 tput cnorm
