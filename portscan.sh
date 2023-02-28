8ea74c07788df0459cba13124a2830146b19a4c7
SezioS <118485549+sergiovks@users.noreply.github.com>
2023-02-28 17:08:19 +0100

    Update portscan.sh

--- portscan.sh
+++ portscan.sh
@@ -12,7 +12,13 @@ tput civis
 read -p "Enter the IP address that you want to scan: " ip
 
 for port in $(seq 1 65535); do
-        timeout 1 bash -c "echo '' > /dev/tcp/$ip/$port" 2>/dev/null && echo "[+] The port $port is OPEN" &
+        timeout 1 bash -c "echo '' > /dev/tcp/$ip/$port" 2>/dev/null && echo "[+] The TCP port $port is OPEN" &
+done; wait
+
+tput cnorm
+
+for port in $(seq 1 65535); do
+        timeout 1 bash -c "echo '' > /dev/udp/$ip/$port" &>/dev/null && echo "[+] The UDP port $port is OPEN" &
 done; wait
 
 tput cnorm
